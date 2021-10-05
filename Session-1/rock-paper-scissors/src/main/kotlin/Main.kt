ead9171dc76b99d578ef574638d62fa1fe96cdf2
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2021-10-05 13:00:04 +0200

    Started extra's on rock, paper, scissors
    Boolean error in code

--- Session-1/rock-paper-scissors/src/main/kotlin/Main.kt
+++ Session-1/rock-paper-scissors/src/main/kotlin/Main.kt
@@ -1,28 +1,40 @@
 fun main(){
+    val repeat: Boolean = true
+    runGame(repeat)
+}
 
-    val options = arrayOf("rock", "paper", "scissors")
+fun runGame(repeat: Boolean?){
+    do {
+        val options = arrayOf("rock", "paper", "scissors")
 
-    var choiceComputer = options.random()
-    println("Type your choice:")
-    val userChoice = readLine()
+        var choiceComputer = options.random()
+        println("Type your choice:")
+        val userChoice = readLine()
 
-    println("Computer: $choiceComputer")
+        println("Computer: $choiceComputer")
 
-    if (choiceComputer == "rock" && userChoice == "paper"){
-        println("User won!")
-    } else if(choiceComputer == "rock" && userChoice == "scissors"){
-        println("Computer won!")
-    } else if(choiceComputer == "paper" && userChoice == "scissors"){
-        println("User won!")
-    } else if(choiceComputer == "paper" && userChoice == "rock"){
-        println("Computer won!")
-    } else if(choiceComputer == "scissors" && userChoice == "rock"){
-        println("User won!")
-    } else if(choiceComputer == "scissors" && userChoice == "paper"){
-        println("Computer won!")
-    } else if (choiceComputer == userChoice){
-        println("Draw!")
-    }else {
-        println("No correct input")
-    }
+        if (choiceComputer == "rock" && userChoice == "paper"){
+            println("User won!")
+        } else if(choiceComputer == "rock" && userChoice == "scissors"){
+            println("Computer won!")
+        } else if(choiceComputer == "paper" && userChoice == "scissors"){
+            println("User won!")
+        } else if(choiceComputer == "paper" && userChoice == "rock"){
+            println("Computer won!")
+        } else if(choiceComputer == "scissors" && userChoice == "rock"){
+            println("User won!")
+        } else if(choiceComputer == "scissors" && userChoice == "paper"){
+            println("Computer won!")
+        } else if (choiceComputer == userChoice){
+            println("Draw!")
+        }else {
+            println("No correct input")
+        }
+
+        println("Want to play again?")
+        val userRepeat = readLine()
+        if (userRepeat == "no"){
+            repeat = !repeat
+        }
+    } while (repeat == true)
 }
